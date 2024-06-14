b70ec2b431d70383e1a76eb6bc075295d07f3bb8
rafael-mansur <rafaelmansur340@gmail.com>
2024-06-14 19:58:34 -0300

    1 commit

---
+++ src/components/Footer/index.jsx
@@ -0,0 +1,9 @@
+import styles from "./Footer.module.css";
+function Footer() {
+    return (
+        <footer className={styles.footer}>
+            <h2>MansurFlix &copy; Desenvolvido por "Rafael Mansur" 2024</h2>
+        </footer>
+    );
+}
+export default Footer;
