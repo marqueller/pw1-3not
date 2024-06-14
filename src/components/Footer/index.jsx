0b30ea201c8b37997c52972e449f875b4108901c
maisamelo023 <melomaisa493@gmail.com>
2024-06-14 20:18:19 -0300

    1 commit

---
+++ src/components/Footer/index.jsx
@@ -0,0 +1,9 @@
+import styles from "./Footer.module.css";
+function Footer() {
+    return (
+        <footer className={styles.footer}>
+            <h2>MeloFlix &copy; Desenvolvido por "Maisa melo" 2024</h2>
+        </footer>
+    );
+}
+export default Footer;
