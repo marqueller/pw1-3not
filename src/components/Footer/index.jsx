34db4280a80eae635e3b083efbdc2630c391296a
luankuhnen <luankuhnen22@gmail.com>
2024-06-14 19:54:48 -0300

    1 commit

---
+++ src/components/Footer/index.jsx
@@ -0,0 +1,9 @@
+import styles from "./Footer.module.css";
+function Footer() {
+    return (
+        <footer className={styles.footer}>
+            <h2>CedupFlix &copy; Desenvolvido por "Luan" 2024</h2>
+        </footer>
+    );
+}
+export default Footer;
