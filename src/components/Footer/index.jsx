4e24de3d689560a65f1349db8272b97fd663ab52
0Kwa <cauassaa47@gmail.com>
2024-06-20 21:31:10 -0300

    1° commit

---
+++ src/components/Footer/index.jsx
@@ -0,0 +1,9 @@
+import styles from "./Footer.module.css";
+function Footer() {
+    return (
+        <footer className={styles.footer}>
+            <h2>CedupFlix &copy; Desenvolvido por "Cauã" 2024</h2>
+        </footer>
+    );
+}
+export default Footer;
