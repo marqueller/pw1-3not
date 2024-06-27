5edf09a9aff4ee77232b7ecb11de08d7cb5a5fc7
Luis Gustavo <gustavelomello1490@gmail.com>
2024-06-27 19:05:18 -0300

    1º Commit - Cedupflix

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -2,7 +2,7 @@ import styles from "./Footer.module.css";
 function Footer() {
     return (
         <footer className={styles.footer}>
-            <h2 class="titulofooter">MarceloFlix &copy; Desenvolvido por <span>Marcelo Filho</span> 2024</h2>
+            <h2 class="titulofooter">LuisFlix &copy; Desenvolvido por <span>Luis</span> 2024</h2>
         </footer>
     );
 }
--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -3,7 +3,7 @@ import styles from "./Header.module.css";
 function Header() {
     return (
         <header className={styles.header}>
-            <span>MarceloFlix</span>
+            <span>LuisFlix</span>
             <nav>
                 <a href="#">Home</a>
                 <a href="#">Assistir</a>
